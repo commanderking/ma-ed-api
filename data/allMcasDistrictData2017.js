21450aff9a959049879eb690973e78e36da76b26
commanderking <jek582@mail.harvard.edu>
2018-04-02 22:14:18 -0400

    add more student groupings

--- data/allMcasDistrictData2017.js
+++ data/allMcasDistrictData2017.js
@@ -1,7 +1,11 @@
 const mcasDistrictAll2017Data = require('./mcasDistrictAll2017');
 const mcasDistrictEconomicallyDisadvantaged2017Data = require('./mcasDistrictEconomicallyDisadvantaged2017');
+const mcasDistrictHighNeeds2017 = require('./mcasDistrictHighNeeds2017');
+const mcasDistrictBlack2017 = require('./mcasDistrictBlack2017');
 
 module.exports = [
   ...mcasDistrictAll2017Data,
-  ...mcasDistrictEconomicallyDisadvantaged2017Data
+  ...mcasDistrictEconomicallyDisadvantaged2017Data,
+  ...mcasDistrictHighNeeds2017,
+  ...mcasDistrictBlack2017
 ]
